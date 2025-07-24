460321a1c2fec489be4b8231a076cfb61648d139
Shashwat <anshuphysic07@gmail.com>
2025-07-24 14:12:11 +0530

    done fixing

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors(
+  {origin:"http://localhost:5173/",}
+));
 app.use(express.json());
 
 // Routes
